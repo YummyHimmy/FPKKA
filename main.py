c63d8cbbd2858e850d3309354ec103bd72dbad2d
justinvalentino <justinvalentino88@gmail.com>
2025-12-13 11:27:52 +0700

    divide and organize the feature: main.py, map.py, settings.py

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import pygame
 import os
 import random
 import sys
-
-# GAME CONFIGURATIONS
-TILE_SIZE = 64
-GRID = 6 # updated the grid dari 7 ke 6
-WIDTH = GRID * TILE_SIZE
-HEIGHT = GRID * TILE_SIZE
-ASSET_DIR = os.path.join(os.path.dirname(__file__), "assets")  # relative assets/
-WALL_SEALED = 4
-
+from settings import *
+import map
 
 # HELPER FUNCTION
 # load asset dari folder asset
@@ -62,140 +55,6 @@ if player_img is None:
 if ghost_img is None:
     ghost_img = solid_surface((200, 60, 140))
 
-# Konstanta tile
-FLOOR = 0
-WALL = 1
-SEALED_FLOOR = 2
-MANUSCRIPT = 3
-MANUSCRIPT_SEALED= 5 # NEW constant biar manuskrip dapat di SEALED_FLOOR
-
-# so there is a case where the
-# sprite (player/ghost) spawned in a "chamber"
-# that isolates them from playing
-def is_map_valid(grid):
-    walkable_tiles=[]
-    for r in range(GRID):
-        for c in range(GRID):
-            # player bisa berjalan di Regular Floor, Sealed Floor, Manuscript
-            if grid[r][c] in [FLOOR, SEALED_FLOOR, MANUSCRIPT, MANUSCRIPT_SEALED]: # NEW MANUSCRIPT_SEALED
-                walkable_tiles.append((r, c))
-    
-    if not walkable_tiles:
-        return False
-
-    # inisiasi Flood Fill
-    start_pos = walkable_tiles[0]
-    queue = [start_pos]
-    visited = {start_pos}
-    count = 0
-
-    while queue:
-        r, c = queue.pop(0)
-        count += 1
-
-        # check 4 area (up, down, left, right)
-        neighbors = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
-        for nr, nc in neighbors:
-            if 0 <= nr < GRID and 0 <= nc < GRID:
-                
-                if (nr, nc) not in visited and grid[nr][nc] in [FLOOR, SEALED_FLOOR, MANUSCRIPT, MANUSCRIPT_SEALED]: # NEW MANUSCRIPT_SEALED    
-                    visited.add((nr, nc))
-                    queue.append((nr, nc))
-    return count == len(walkable_tiles)
-
-# UPDATE: terdapat sebuah clumps 2x2, ini sebagai pencegah penumpukan
-def causes_clump(grid, r, c):
-    # cek tile apakah tembol
-    def is_wall(nr, nc):
-        if 0 <= nr < GRID and 0 <= nc < GRID:
-            return grid[nr][nc] in [WALL, WALL_SEALED]
-        return True # UPDATE: it wouldn't want to stick to wall
-
-    # check directions clumping 2x2
-    checks = [
-        [(r-1, c), (r, c-1), (r-1, c-1)], # NW
-        [(r-1, c), (r, c+1), (r-1, c+1)], # NE
-        [(r+1, c), (r, c-1), (r+1, c-1)], # SW
-        [(r+1, c), (r, c+1), (r+1, c+1)]  # SE
-    ]
-
-    for check in checks:
-        if all(is_wall(nr, nc) for nr, nc in check):
-            return True # Terdeteksi akan membentuk kotak 2x2!
-            
-    return False
-
-# MAP GENERATOR
-def generate_map():
-    while True:
-    # insisiasi map structure dasar
-        grid = [[FLOOR for _ in range(GRID)] for _ in range(GRID)]
-
-    # random wall placement
-        total_walls = random.randint(10, 16) # update dari 8,14 ke 10,16
-        placed = 0
-        attempts = 0
-        while placed < total_walls and attempts < 200:
-            attempts += 1
-            r = random.randint(0, GRID - 1)
-            c = random.randint(0, GRID - 1)
-
-            # menghindari sealed atau manuskrip terlalu awal
-
-            # UPDATE: jika 70% WALL 30% SEALED_WALL
-            if grid[r][c] == FLOOR:
-                if causes_clump(grid, r, c):
-                    continue
-                rng = random.random()
-                if rng < 0.7:
-                    grid[r][c] = WALL
-                else:
-                    grid[r][c] = WALL_SEALED
-                placed += 1
-
-    # NOTES: diubah, tidak ada minimal untuk sealed floor
-    # menaruh 3 sealed floor secara random
-        sealed_positions = []
-        attempts = 0
-        while len(sealed_positions) < 3 and attempts < 200:
-            attempts += 1
-            r = random.randint(0, GRID - 1)
-            c = random.randint(0, GRID - 1)
-            if grid[r][c] == FLOOR:
-                grid[r][c] = SEALED_FLOOR
-                sealed_positions.append((r, c))
-
-    # menaruh 3 mansuksrip secara random
-        manuscript_positions = []
-        attempts = 0
-        while len(manuscript_positions) < 3 and attempts < 500:
-            attempts += 1
-            r = random.randint(0, GRID - 1)
-            c = random.randint(0, GRID - 1)
-            if grid[r][c] == FLOOR:
-                grid[r][c] = MANUSCRIPT
-                manuscript_positions.append((r, c))
-            if grid[r][c] == SEALED_FLOOR:              # NEW MANUSCRIPT_SEALED
-                grid[r][c] = MANUSCRIPT_SEALED
-                manuscript_positions.append((r, c))
-
-        # UPDATE: sebelum menempatkan player, cek valid atau tidak mapnya,
-        # sebelumnya diemplementasikan attempt menebak penempatan dengan looping 500x player di floor 
-        if is_map_valid(grid):
-            # checks out the safest spot, penetapannya di Regular Floor
-            safe_spots = []
-            for r in range(GRID):
-                for c in range(GRID):
-                    if grid[r][c] == FLOOR:
-                        safe_spots.append((r,c))
-            
-            if len(safe_spots) >= 2:    # untuk spot Player & Ghost
-                player_pos = random.choice(safe_spots)
-                safe_spots.remove(player_pos)
-                ghost_pos = random.choice(safe_spots)
-
-                return grid, player_pos, ghost_pos
-
 # DRAW THE GAME TO THE WINDOW
 def draw(grid, player_pos, ghost_pos, manuscripts_left):
     # the loop will draw seluruh grid pada map
@@ -231,9 +90,9 @@ def draw(grid, player_pos, ghost_pos, manuscripts_left):
     screen.blit(txt, (4, 4))
 
 # MAIN
-grid, player_pos, ghost_pos = generate_map()
+grid, player_pos, ghost_pos = map.generate_map()
 # menghitung sisa dari manuscript yang belum diambil
-manuscripts_left = sum(1 for r in range(GRID) for c in range(GRID) if grid[r][c] == MANUSCRIPT)
+manuscripts_left = sum(1 for r in range(GRID) for c in range(GRID) if grid[r][c] in [MANUSCRIPT, MANUSCRIPT_SEALED])
 
 # berjalannya game
 running = True
@@ -243,7 +102,7 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
-                grid, player_pos, ghost_pos = generate_map()
+                grid, player_pos, ghost_pos = map.generate_map()
                 # UPDATE: there was a bug of which was just counting manuscript in Regular Floor
                 manuscripts_left = sum(1 for r in range(GRID) for c in range(GRID) if grid[r][c] in [MANUSCRIPT, MANUSCRIPT_SEALED])
             if event.key == pygame.K_ESCAPE:
